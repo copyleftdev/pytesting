75c634bc1151aeee9e967754a1c740848c947e8b
Don Johnson <dj@codetestcode.io>
2014-11-09 10:33:21 -0800

    added Coverage exclusion &  Logic for  non integer  logic

--- app/calculate.py
+++ app/calculate.py
@@ -1,9 +1,12 @@
 class Calculate(object):
   """docstring for Calculate"""
   def add(self,x , y):
-    return x + y
+    if type(x) == int and type(y) == int:
+      return x + y
+    else:
+      TypeError("Invalid type: {} and {}".format(type(x),type(y)))
 
-if __name__ == '__main__':
-  calc = Calculate()
-  result = calc.add(2,2)
-  print result
+if __name__ == '__main__': #pragma: no cover
+  calc = Calculate() #pragma: no cover
+  result = calc.add(2,2) #rpagma: no cover
+  print result  #pragma: no cover
